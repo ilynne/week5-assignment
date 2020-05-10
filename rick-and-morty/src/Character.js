69f8a6238b133538bb8b7df2ebc4f0fec909b936
Lynne Cooney <ilynne@gmail.com>
2020-05-10 08:17:41 -0700

    index layout in good shape

--- rick-and-morty/src/Character.js
+++ rick-and-morty/src/Character.js
@@ -3,11 +3,14 @@ import React from 'react'
 export default class Character extends React.Component {
 
   render() {
-    const { id, name, image } = this.props.character
+    const { id, name, image, created } = this.props.character
     return (
       <div className={'character-container'}>
-        <img src={image} alt={`portrait of ${name}`}></img>
-        <p>{name}</p>
+        <div className={'character-image-container'}>
+          <img src={image} alt={`portrait of ${name}`}></img>
+          <p className={'image-time-stamp'}>{created}</p>
+        </div>
+        <div className={'caption'}>{name}</div>
       </div>
     )
   }

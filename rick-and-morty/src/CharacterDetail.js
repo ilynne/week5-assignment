6110c87177436aec2580118464621ad9797f6928
Lynne Cooney <ilynne@gmail.com>
2020-05-10 17:33:05 -0700

    final character layout

--- rick-and-morty/src/CharacterDetail.js
+++ rick-and-morty/src/CharacterDetail.js
@@ -22,9 +22,10 @@ export default class CharacterDetail extends React.Component {
   }
 
   render() {
-    const { name, image, created, species, gender, status, location, origin } = this.state.response;
-    const locationName = location ? location.name : 'Unknown';
-    const originName = origin ? origin.name : 'Unknown';
+    const { name, image, created, species, gender, status, location, origin, episode } = this.state.response;
+    const locationName = location ? location.name : 'unknown';
+    const originName = origin ? origin.name : 'unknown';
+    const episodeCount = episode ? episode.length : 'unknown';
 
     return (
       <div className={'character-detail-container'}>
@@ -35,42 +36,52 @@ export default class CharacterDetail extends React.Component {
         >
         </CharacterPolaroid>
         <div className={'character-information-container'}>
-          <table className={'character-information-form'}>
-            <tbody>
-              <tr>
-                <td colSpan={'3'}>
-                  <span className="form-label">name</span>
-                  <span className="form-info">{name}</span>
-                </td>
-              </tr>
-              <tr>
+          <div className={'character-information-form-container'}>
+            <h2>Character Data Sheet</h2>
+            <table className={'character-information-form'}>
+              <tbody>
+                <tr>
+                  <td colSpan={'3'}>
+                    <span className="form-label">name</span>
+                    <span className="form-info">{name}</span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <span className="form-label">species</span>
+                    <span className="form-info">{species}</span>
+                  </td>
+                  <td>
+                    <span className="form-label">gender</span>
+                    <span className="form-info">{gender}</span>
+                  </td>
+                </tr>
+                <tr>
                 <td>
-                  <span className="form-label">species</span>
-                  <span className="form-info">{species}</span>
-                </td>
-                <td>
-                  <span className="form-label">gender</span>
-                  <span className="form-info">{gender}</span>
-                </td>
-                <td>
-                  <span className="form-label">status</span>
-                  <span className="form-info">{status}</span>
-                </td>
-              </tr>
-              <tr>
-                <td colSpan={'3'}>
-                  <span className="form-label">location</span>
-                  <span className="form-info">{locationName}</span>
-                </td>
-              </tr>
-              <tr>
-                <td colSpan={'3'}>
-                  <span className="form-label">origin</span>
-                  <span className="form-info">{originName}</span>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+                    <span className="form-label">episodes</span>
+                    <span className="form-info">{episodeCount}</span>
+                  </td>
+                  <td>
+                    <span className="form-label">status</span>
+                    <span className="form-info">{status}</span>
+                  </td>
+                </tr>
+                <tr>
+                  <td colSpan={'2'}>
+                    <span className="form-label">location</span>
+                    <span className="form-info">{locationName}</span>
+                  </td>
+                </tr>
+                <tr>
+                  <td colSpan={'2'}>
+                    <span className="form-label">origin</span>
+                    <span className="form-info">{originName}</span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <p className={'form-id'}>RICK AND MORTY CHARACTER DATA FORM ID #2020-42</p>
+          </div>
         </div>
       </div>
     )

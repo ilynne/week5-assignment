e1705454e8912f7f178ca27d4a49177826cd9757
Lynne <lynne@Kotiss-MacBook-Pro.local>
2020-05-17 21:26:46 -0700

    Updates

---
+++ precache-manifest.4440cf73d157c6936ef3ed2b919e18c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f2a4eaf0e98847410096de48f506cd0",
+    "url": "/rick-and-morty/index.html"
+  },
+  {
+    "revision": "bad47dd2ced81c70d683",
+    "url": "/rick-and-morty/static/css/main.fcb15aac.chunk.css"
+  },
+  {
+    "revision": "a70faa3077b34ae02032",
+    "url": "/rick-and-morty/static/js/2.f48b43b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rick-and-morty/static/js/2.f48b43b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bad47dd2ced81c70d683",
+    "url": "/rick-and-morty/static/js/main.3470e93a.chunk.js"
+  },
+  {
+    "revision": "94eafa71cab9436efa5b",
+    "url": "/rick-and-morty/static/js/runtime-main.5ff325dc.js"
+  }
+]);

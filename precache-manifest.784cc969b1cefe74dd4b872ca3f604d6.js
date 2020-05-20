32c5d0a774c61feeffc4ab294710e352be8daa07
Lynne <lynne@Kotiss-MacBook-Pro.local>
2020-05-19 17:49:19 -0700

    Updates

--- precache-manifest.784cc969b1cefe74dd4b872ca3f604d6.js
+++ precache-manifest.784cc969b1cefe74dd4b872ca3f604d6.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "34851cfbfd16416c0d0eed37cd334e34",
+    "revision": "3bd1a6ddccbe124d130e85b589894609",
     "url": "/week5-assignment/index.html"
   },
   {
-    "revision": "6bfdd311d5a9c0c98bdf",
+    "revision": "a995f8c5debd7ec95de0",
     "url": "/week5-assignment/static/css/main.fcb15aac.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/week5-assignment/static/js/2.f48b43b6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6bfdd311d5a9c0c98bdf",
-    "url": "/week5-assignment/static/js/main.b5dec190.chunk.js"
+    "revision": "a995f8c5debd7ec95de0",
+    "url": "/week5-assignment/static/js/main.d84fc969.chunk.js"
   },
   {
     "revision": "8e4f94518f34db53baca",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/week5-assignment/precache-manifest.8464bb9ba2b930b73acea7fc937eddf9.js"
+  "/week5-assignment/precache-manifest.784cc969b1cefe74dd4b872ca3f604d6.js"
 );
 
 self.addEventListener('message', (event) => {
